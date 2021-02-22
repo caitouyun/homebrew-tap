0b3b570a09e03d6ca2e35792b7b41dc6c62f6416
Wang Zuo <wzuoadjusted@gmail.com>
2021-02-22 12:09:40 +0800

    add caitou.rb

---
+++ caitou.rb
@@ -0,0 +1,9 @@
+class Caitou < Formula
+  desc "caitouyun cli"
+  homepage "https://caitouyun.com"
+  url "http://cli.caitouyun.com/caitou-darwin-amd64"
+
+  def install
+    bin.install "caitou-darwin-amd64"
+  end
+end
